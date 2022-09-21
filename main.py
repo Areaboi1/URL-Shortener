545ff79ab434979c0a5ca933b0a1d5f0a4f38c5b
Areaboi1 <aryamanpal40@gmail.com>
2022-09-21 12:43:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 app=Flask(__name__)
 @app.route('/')
+def hello_world():
     return("Hello WOrld")
 
 @app.route('/arya')
 def hello_anish():
     return("Hi Aryaman")
 
+if (__name__)=="__main__":
     app.run(port=5000,debug="True")
